2db0626a4e8dc4d5797f4b0ba131786a8c1d92cb
leo_cucho <leonardocucho@gmail.com>
2021-04-06 12:59:16 -0500

    newgeneration

--- src/components/SegundaVuelta.vue
+++ src/components/SegundaVuelta.vue
@@ -1,11 +1,29 @@
 <template>  
     <div class="row">
-        <div class="col-12">
+        <div class="col-4">
           <ul> 
+            <h5>Candidato</h5>
             <li :key="eleccion.validos" v-for="eleccion in segunda_vuelta">
-              {{ eleccion }}
+              {{eleccion.candidato}} 
+            </li>
+            
+          </ul>
+        </div>
+        <div class="col-4">
+          <ul>
+            <h5>Partido</h5>
+            <li :key="eleccion.validos" v-for="eleccion in segunda_vuelta">
+                {{eleccion.partido}} 
             </li>
           </ul>
+        </div>
+        <div class="col-4">          
+          <ul>
+            <h5>Votos validos</h5>
+            <li :key="eleccion.validos" v-for="eleccion in segunda_vuelta">
+              {{eleccion.total_votos}} 
+            </li>
+          </ul> 
         </div>
     </div>
 </template>
