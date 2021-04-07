3aa01357f8ccc09c77d5e21019007f27ce6c9067
Antonio Cucho <antoniocuga@gmail.com>
2021-04-06 20:14:24 -0400

    segunda vuelta

--- src/components/SegundaVuelta.vue
+++ src/components/SegundaVuelta.vue
@@ -5,44 +5,54 @@
       <div class="col-12 mt-3 border-bottom">
         <h3>Segunda Vuelta</h3>        
       </div>
-            
+    </div>
+
+    <div class="row">
+      <div class="col-12 flex-row" :key="eleccion" v-for="(items, eleccion) in candidatos_segunda">
+        
         <div class="row">
-          <div class="col-12 flex-row" :key="eleccion.validos" v-for="eleccion in segunda_vuelta">
+          <div class="col-6" :key="candidato.candidato_id" v-for="candidato in items">
+
             <div class="candidate-info historico align-self-center">
               <div class="">
-                <h4>{{eleccion.eleccion}}</h4>
+                <h4>{{candidato.eleccion}}</h4>
               </div>
               <div class="">
-                <h4>{{eleccion.candidato}}</h4>
-                <h5>{{eleccion.partido}}</h5>
+                <h4>{{candidato.candidato}}</h4>
+                <h5>{{candidato.partido}}</h5>
               </div>
             </div>
 
             <div class="candidate-results flex-row">                
               <div class="candidate-bar">
-                <div class="tooltip-c">{{eleccion.total_votos}}</div>                 
+                <div class="tooltip-c">{{candidato.total_votos}}</div>                 
               </div>
-              <div class="tooltip-c"><span>{{eleccion.validos}}</span></div>                            
+              <div class="tooltip-c"><span>{{candidato.validos}}</span></div>                            
             </div>            
+          </div>
+
           </div>
         </div>
 
-      
 
     </div>
+
 
   </div>
 </template>
 
 <script>
 
-  
+  import { groupBy } from 'lodash'
   
   export default {
     name: 'SegundaVuelta.vue',
     computed: {
       segunda_vuelta() {
         return require('../data/segunda_vuelta.json')
+      },
+      candidatos_segunda() {
+        return groupBy(this.segunda_vuelta, 'eleccion')
       }
     }
   }
