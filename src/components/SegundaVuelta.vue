7a64674357034bc8f47e47ddee673d2986c6f158
leo_cucho <leonardocucho@gmail.com>
2021-04-08 11:04:53 -0500

    new

--- src/components/SegundaVuelta.vue
+++ src/components/SegundaVuelta.vue
@@ -10,7 +10,7 @@
           </div>
           <div class="col-12">
             <div class="row" :key="candidato.candidato_id" v-for="candidato in eleccion.items">
-              <div class="col-6 pr-0">
+              <div class="col-12 col-md-6 pr-0">
                 <div class="candidate-info historico align-self-center">
                   <div class="">
                     <img height="40px" width="40px" :src="getImageCandidate(candidato.candidato_id)" />
@@ -21,7 +21,7 @@
                   </div>
                 </div>
               </div>
-              <div class="col-6 pl-0">
+              <div class="col-12 col-md-6 pl-0">
                 <div class="candidate-results">                
                   <div class="candidate-bar">
                     <div class="tooltip-c">{{ candidato.validos+"%" }} </div>
