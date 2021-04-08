393e275059790709e716bbe2eac6f9ea0b8a85c8
leo_cucho <leonardocucho@gmail.com>
2021-04-08 10:12:09 -0500

    new

--- src/components/MapaEleciones.vue
+++ src/components/MapaEleciones.vue
@@ -14,8 +14,7 @@
               <div class="partido-mapa"><img width="25px" :src="getImagePartido(c.partido_id)" /> {{ c.partido }}</div>
 
               <div class="text-center mt-2 mb-2 porcentaje">{{ c.porcentaje }} <span>%</span></div>
-              <div class="diferencia">+ 120,459 diferencia</div>
-              <div class="votos-resultado">{{ c.votos }} total de votos</div>
+              <div class="diferencia">+ 120,459 diferencia</div>              
               <div class="puesto" v-if="ix == 0 || ix == 1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                 <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
               </svg> En segunda vuelta</div>
