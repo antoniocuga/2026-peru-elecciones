04a2456d1bd10e0b1677393dfa9e43f6cfebc25f
Antonio Cucho <antoniocuga@gmail.com>
2021-04-09 14:47:15 -0400

    update componente 2016

--- src/components/candidatosResultados.vue
+++ src/components/candidatosResultados.vue
@@ -52,36 +52,8 @@
     
     <div class="col-12">
 
-      <div class="row resultado-nacional">
+      <elecciones2016 />
 
-        <div class="col-6 titulo text-left">Elecciones 2016 (primera vuelta)</div>
-        <div class="col-6 text-right">
-          <b-button @click="openResultados=!openResultados" class="btn"> {{ openResultados ? 'Cerrar' : 'Ver resultados'}}</b-button>
-        </div>
-        <b-collapse v-model="openResultados" id="resultados2016" class="col-12">
-          <b-tabs>
-            <b-tab class="" title="Datos de la votacion">
-              <div class="row">
-                <div class="col-6 text-left">
-                  <div>Electores habiles</div>        
-                </div>
-                <div class="col-6 text-right">
-                  <div>22901954</div>
-                </div>
-              </div>
-              <div class="row"><div class="col-6">ciudadanos que no votaron</div><div class="col-6">895785</div></div>
-              <div class="row"><div class="col-6">voto en blanco/nulo</div><div class="col-6">votos emitidos</div></div>
-  
-            </b-tab>
-            <b-tab title="Por partidos">
-              <div class="row"><div class="col-6">Partido Morado</div><div class="col-6">12354</div></div>
-              <div class="row"><div class="col-6">Juntos por el Perú</div><div class="col-6">12354</div></div>
-              <div class="row"><div class="col-6">fuerza popular</div><div class="col-6">123654</div></div>
-              <div class="row"><div class="col-6">renovacion popular</div><div class="col-6">12354</div></div>
-            </b-tab>
-          </b-tabs>
-        </b-collapse>
-      </div>
     </div>
   </div>
   
@@ -89,8 +61,7 @@
 
 <script>
 
-  
-  
+  import elecciones2016 from './elecciones2016.vue'
   import { mapState } from 'vuex'
   
   export default {
@@ -98,10 +69,12 @@
     props: {
       candidatos: Array
     },
+    components: {
+      elecciones2016
+    },
     data() {
       return {
-        open: false,
-        openResultados: false
+        open: false
       }
     },
     computed: {
