73b4bf3d92e01f770e3e5e0dc33db4d1700d507a
Antonio Cucho <antoniocuga@gmail.com>
2026-02-27 08:04:52 -0500

    removing crawlerr files

--- crawler/onpe/download.sh
+++
@@ -1,4 +0,0 @@
-while IFS=',' read -r guid field
-do
-    curl 'https://api.resultados.eleccionesgenerales2021.pe/results/10/'$guid'?name=param' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-US,en;q=0.5' --output 'regiones/'$guid'.json'
-done < departamentos_onpe.csv
